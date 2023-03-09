7241d64f2d384366ec5ba19705a5f208c43e053f
Sanya <yyf.yyff@mail.ru>
2023-03-09 21:38:13 +0300

    Header

--- diplom-react/src/App.js
+++ diplom-react/src/App.js
@@ -1,28 +1,28 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
 
-import Header from "./components/Header.jsx";
+import Header from './components/Header.jsx';
 
-import Home from "./pages/Home.jsx";
+import Home from './pages/Home.jsx';
 
-import "./scss/app.scss";
+import './scss/app.scss';
 
 function App() {
   const routes = useRoutes([
     //ruoter константа
     {
-      path: "/",
+      path: '/',
       element: <Home />, //домашняя страница
     },
   ]);
 
   return (
-    <div className="wrapper">
-      <Header/>
-      <div className="content">
-        {routes /*Переход по ссылкам через router константу*/}
+    <>
+      <Header />
+      <div className="wrapper">
+        <div className="content">{routes /*Переход по ссылкам через router константу*/}</div>
       </div>
-    </div>
+    </>
   );
 }
 
