ade7e2bf199d24859a192ba64af79b59679fe154
Sanya <yyf.yyff@mail.ru>
2023-03-26 22:39:56 +0300

    111

--- diplom-react/src/pages/Info.jsx
+++ diplom-react/src/pages/Info.jsx
@@ -10,22 +10,23 @@ const Info = () => {
     <>
       <Container>
         <Corousel1 />
+
         <h2>Преимущества</h2>
         <Row className="justify-content-center">
           <div className="card col-3">
-            <img src={Img1} alt="xyi" />
+            <img src={Img1} alt="image" />
             <div className="card-body">
               <h5 className="card-title">Ассортимент более 60000 товаров</h5>
             </div>
           </div>
           <div className="card  col-3">
-            <img src={Img2} alt="xyi" />
+            <img src={Img2} alt="image" />
             <div className="card-body">
               <h5 className="card-title">Акции и скидки</h5>
             </div>
           </div>
           <div className="card col-3">
-            <img src={Img3} alt="xyi" />
+            <img src={Img3} alt="image" />
             <div className="card-body">
               <h5 className="card-title">2 аптеки в одном регионе</h5>
             </div>
