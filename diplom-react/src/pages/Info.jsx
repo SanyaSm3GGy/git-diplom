7eedf0b0ee3cd2e660e271499d48b21fb10c7978
danil <danil.ruskek@gmail.com>
2023-03-26 22:14:41 +0300

    556

--- diplom-react/src/pages/Info.jsx
+++ diplom-react/src/pages/Info.jsx
@@ -1,5 +1,38 @@
-
-function Info() {
-    return
-}
+import React from 'react';
+import Corousel1 from '../components/corousel/Corousel1';
+import { Row } from 'react-bootstrap';
+import Img1 from '../assets/img/41.png';
+import Img2 from '../assets/img/42.png';
+import Img3 from '../assets/img/43.png';
+import Container from 'react-bootstrap/Container';
+const Info = () => {
+  return (
+    <>
+      <Container>
+        <Corousel1 />
+        <h2>Преимущества</h2>
+        <Row className="justify-content-center">
+          <div className="card col-3">
+            <img src={Img1} alt="xyi" />
+            <div className="card-body">
+              <h5 className="card-title">Ассортимент более 60000 товаров</h5>
+            </div>
+          </div>
+          <div className="card  col-3">
+            <img src={Img2} alt="xyi" />
+            <div className="card-body">
+              <h5 className="card-title">Акции и скидки</h5>
+            </div>
+          </div>
+          <div className="card col-3">
+            <img src={Img3} alt="xyi" />
+            <div className="card-body">
+              <h5 className="card-title">2 аптеки в одном регионе</h5>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+};
 export default Info;
