9a44745b80918997ee63f2d877672c65b7c711d1
Sanya <yyf.yyff@mail.ru>
2023-03-29 23:01:09 +0300

    123

--- diplom-react/src/components/CardItem/CardItem.jsx
+++ diplom-react/src/components/CardItem/CardItem.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import NotFound from '../../assets/img/NotFound.jpg';
 
 const CardItem = ({ name, price, img, have, description1, description2, description3 }) => {
+  const loadedImage = () => {
+    if (img) {
+      return img;
+    } else {
+      return NotFound;
+    }
+  };
   return (
     <div>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={img} />
+        <Card.Img variant="top" src={loadedImage()} />
         <Card.Body>
           <Card.Title className="bold-title">{name}</Card.Title>
           <Card.Text>
-            {description1}
+            Производитель: {description1}
             <br />
-            {description2}
+            Форма выпуска: {description2}
             <br />
-            {description3}
+            Кол-во в упаковке: {description3}
             <br />
           </Card.Text>
-          <Card.Text>{have}</Card.Text>
+          <Card.Text>В наличии: {have}</Card.Text>
           <Button className="button">{price}</Button>
         </Card.Body>
       </Card>
