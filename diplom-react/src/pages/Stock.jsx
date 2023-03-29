8d5de0a49da61098039d3a47c3718b6c19c4fcf0
danil <danil.ruskek@gmail.com>
2023-03-29 22:46:48 +0300

    777

---
+++ diplom-react/src/pages/Stock.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+const Stock = () => {
+    return (
+    <>
+   <Container>
+  <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/">Главная</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Каталог
+            </li>
+          </ol>
+        </nav>
+        <div>
+      <h1>
+        Акции <Badge bg="secondary">Новинка</Badge>
+      </h1>
+      </div>
+   </Container> 
+   </>         
+);
+};
+export default Stock;
+https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload/site_specials/c6/23/c62369c1da01c9fbd779f8b02b7e3dbeea2571c362de62e39446bf85c79b81f8.jpg
+https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload/site_specials/b0/69/b069200ff1e11e0d7728bccfdb6179f8c0002381d9b4ac6e18231fe467a1ce8a.jpg
+https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload/site_specials/b6/7e/b67e3c4168b45ff5f119e653bcf3b3508488b1522d33011a17600d7709523878.jpg
