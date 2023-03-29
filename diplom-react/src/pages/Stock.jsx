1266287aa4942e15af09622eb237709f81a53d24
Sanya <yyf.yyff@mail.ru>
2023-03-29 23:07:08 +0300

    123

--- diplom-react/src/pages/Stock.jsx
+++ diplom-react/src/pages/Stock.jsx
@@ -4,10 +4,10 @@ import { Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Badge from 'react-bootstrap/Badge';
 const Stock = () => {
-    return (
+  return (
     <>
-   <Container>
-  <nav aria-label="breadcrumb">
+      <Container>
+        <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
             <li className="breadcrumb-item">
               <Link to="/">Главная</Link>
@@ -18,15 +18,12 @@ const Stock = () => {
           </ol>
         </nav>
         <div>
-      <h1>
-        Акции <Badge bg="secondary">Новинка</Badge>
-      </h1>
-      </div>
-   </Container> 
-   </>         
-);
+          <h1>
+            Акции <Badge bg="secondary">Новинка</Badge>
+          </h1>
+        </div>
+      </Container>
+    </>
+  );
 };
 export default Stock;
-https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload/site_specials/c6/23/c62369c1da01c9fbd779f8b02b7e3dbeea2571c362de62e39446bf85c79b81f8.jpg
-https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload/site_specials/b0/69/b069200ff1e11e0d7728bccfdb6179f8c0002381d9b4ac6e18231fe467a1ce8a.jpg
-https://12bb6ecf-bda5-4c99-816b-12bda79f6bd9.selcdn.net/upload/site_specials/b6/7e/b67e3c4168b45ff5f119e653bcf3b3508488b1522d33011a17600d7709523878.jpg
